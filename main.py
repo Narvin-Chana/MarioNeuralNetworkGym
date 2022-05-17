06cf91e69d93315a7d2439987781e4451c2b16f2
Noé <noepec1@gmail.com>
2022-05-17 09:33:50 +0200

    fix: Change SIMPLE_MOVEMENT to COMPLEX_MOVEMENT

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 from network import *
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
 
 def main():
