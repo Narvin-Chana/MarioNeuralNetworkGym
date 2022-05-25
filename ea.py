391bfe3affa07dab9c7b15ab7a1ea16603d35a14
Noé <noepec1@gmail.com>
2022-05-25 14:21:59 +0200

    feat: Logging and Statistics of EA

--- ea.py
+++ ea.py
@@ -1,7 +1,8 @@
 import datetime
 
 import collections.abc
-#scoop needs the four following aliases to be done manually.
+
+# scoop needs the four following aliases to be done manually.
 collections.Iterable = collections.abc.Iterable
 collections.Mapping = collections.abc.Mapping
 collections.MutableSet = collections.abc.MutableSet
@@ -14,25 +15,26 @@ import gym_super_mario_bros
 
 from wrappers import wrapper
 
-NB_GEN = 1000
-POP_SIZE = 100
+SAVE_INTERVAL = 10
+
+NB_GEN = 1
+POP_SIZE = 8
 IND_SIZE = 3000
 NB_ACTIONS = 5
 
 IND_CROSSOVER_RATE = 0.5
-IND_MUTATION_RATE = 0.3
+IND_MUTATION_RATE = 0.2
 GENE_MUTATION_RATE = 100 / IND_SIZE
 
 SELECT_SIZE = 10
 
 CUSTOM_MOVEMENT = [["NOOP"], ["right", "A", "B"], ["right", "B"], ["left", "A", "B"], ["left", "B"]]
 
-if __name__ != "__main__":
-    env = gym_super_mario_bros.make('SuperMarioBros-v0')
-    env = JoypadSpace(env, CUSTOM_MOVEMENT)
-    # Applies custom wrappers to the environment.
-    frameSkipCount = 4
-    env = wrapper(env, frameSkipCount)
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+env = JoypadSpace(env, CUSTOM_MOVEMENT)
+# Applies custom wrappers to the environment.
+frameSkipCount = 4
+env = wrapper(env, frameSkipCount)
 
 
 def evaluate_individual(ind):
@@ -59,9 +61,28 @@ creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 if __name__ == "__main__":
+    import os
+    import time
     import numpy
     from deap import creator
     from numpy import random
+    import pickle
+
+    RESULTS_DIR = "./results/{0}/"
+    result_name = datetime.datetime.now().strftime("%H-%M-%S")
+
+    dir_name = RESULTS_DIR.format(result_name)
+    path = dir_name + f"{result_name}." + "{}"
+    os.makedirs(dir_name, exist_ok=True)
+
+
+    def pickle_objects(objects):
+        for (ext, o) in objects:
+            with open(path.format(ext), "wb") as f:
+                pickle.dump(o, f)
+
+
+    start = time.time()
 
     toolbox = base.Toolbox()
     toolbox.register("attr_action", random.randint, 0, NB_ACTIONS)
@@ -73,20 +94,30 @@ if __name__ == "__main__":
     toolbox.register("evaluate", evaluate_individual)
     toolbox.register("map", futures.map)
 
-    pop = [toolbox.individual() for _ in range(POP_SIZE)]
-
     stats = tools.Statistics(key=lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
     stats.register("std", numpy.std)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
+
+    logbook = tools.Logbook()
     hof = tools.HallOfFame(10)
+    history = tools.History()
+    toolbox.decorate("mate", history.decorator)
+    toolbox.decorate("mutate", history.decorator)
+
+    pop = [toolbox.individual() for _ in range(POP_SIZE)]
 
     # Evaluate the individuals with an invalid fitness
     invalid_ind = [ind for ind in pop if not ind.fitness.valid]
     fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
     for ind, fit in zip(invalid_ind, fitnesses):
         ind.fitness.values = fit
+
+    history.update(pop)
+    logbook.record(time=time.time() - start, gen=0, evals=len(invalid_ind), **stats.compile(pop))
+    hof.update(pop)
+    print(logbook.stream)
 
     for g in range(NB_GEN):
         # Select and clone the next generation individuals
@@ -104,8 +135,9 @@ if __name__ == "__main__":
 
         # The population is entirely replaced by the offspring
         pop[:] = offspring
-        print(stats.compile(pop))
+        logbook.record(time=time.time() - start, gen=g + 1, evals=len(invalid_ind), **stats.compile(pop))
+        hof.update(pop)
+        print(logbook.stream)
 
-# with open("results/results_ea_{}.txt".format(datetime.now().strftime("%H-%M-%S")), "w") as fw:
-#    fw.write(str(hof))
-#    fw.write(str(logbook))
+        if g % SAVE_INTERVAL == 0:
+            pickle_objects([("log", logbook), ("hof", hof), ("hist", history)])
