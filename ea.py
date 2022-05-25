cb9796631b29c61d1ca1c6c9b9c6aef860bc0760
Noé <noepec1@gmail.com>
2022-05-25 12:37:45 +0200

    feat: Evolutionary algorithm Multithreading
    To launch with the multithreading, you need to add "-m scoop" as interpreter parameter

--- ea.py
+++ ea.py
@@ -1,33 +1,29 @@
-import time
-from datetime import datetime
-
-import numpy
+from scoop import futures
 from deap import base, creator, tools, algorithms
-from numpy import random
-
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 
 from wrappers import wrapper
 
-NB_GEN = 20000
-POP_SIZE = 25
+NB_GEN = 1000
+POP_SIZE = 100
 IND_SIZE = 3000
 NB_ACTIONS = 5
 
 IND_CROSSOVER_RATE = 0.5
-IND_MUTATION_RAT = 0.1
-GENE_MUTATION_RATE = 1 / IND_SIZE
+IND_MUTATION_RATE = 0.3
+GENE_MUTATION_RATE = 100 / IND_SIZE
 
 SELECT_SIZE = 10
 
 CUSTOM_MOVEMENT = [["NOOP"], ["right", "A", "B"], ["right", "B"], ["left", "A", "B"], ["left", "B"]]
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, CUSTOM_MOVEMENT)
-# Applies custom wrappers to the environment.
-frameSkipCount = 4
-env = wrapper(env, 4)
+if __name__ != "__main__":
+    env = gym_super_mario_bros.make('SuperMarioBros-v0')
+    env = JoypadSpace(env, CUSTOM_MOVEMENT)
+    # Applies custom wrappers to the environment.
+    frameSkipCount = 4
+    env = wrapper(env, frameSkipCount)
 
 
 def evaluate_individual(ind):
@@ -47,35 +43,62 @@ def evaluate_individual(ind):
         current_step += 1
         env.render()
 
+
     return (total_reward,)
 
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
-toolbox = base.Toolbox()
-toolbox.register("attr_action", random.randint, 0, NB_ACTIONS)
-toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.attr_action, n=IND_SIZE)
+if __name__ == "__main__":
+    import numpy
+    from deap import creator
+    from numpy import random
 
-toolbox.register("mate", tools.cxOnePoint)
-toolbox.register("mutate", tools.mutUniformInt, low=0, up=NB_ACTIONS-1, indpb=GENE_MUTATION_RATE)
-toolbox.register("select", tools.selRoulette)
-toolbox.register("evaluate", evaluate_individual)
+    toolbox = base.Toolbox()
+    toolbox.register("attr_action", random.randint, 0, NB_ACTIONS)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_action, n=IND_SIZE)
 
-pop = [toolbox.individual() for _ in range(POP_SIZE)]
+    toolbox.register("mate", tools.cxOnePoint)
+    toolbox.register("mutate", tools.mutUniformInt, low=0, up=NB_ACTIONS - 1, indpb=GENE_MUTATION_RATE)
+    toolbox.register("select", tools.selRoulette)
+    toolbox.register("evaluate", evaluate_individual)
+    toolbox.register("map", futures.map)
 
-stats = tools.Statistics(key=lambda ind: ind.fitness.values)
-stats.register("avg", numpy.mean)
-stats.register("std", numpy.std)
-stats.register("min", numpy.min)
-stats.register("max", numpy.max)
-hof = tools.HallOfFame(10)
-finalpop, logbook = algorithms.eaSimple(pop, toolbox, IND_CROSSOVER_RATE, GENE_MUTATION_RATE, NB_GEN, stats=stats, halloffame=hof, verbose=True)
+    pop = [toolbox.individual() for _ in range(POP_SIZE)]
 
-env.close()
+    stats = tools.Statistics(key=lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+    hof = tools.HallOfFame(10)
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    for g in range(NB_GEN):
+        # Select and clone the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        offspring = map(toolbox.clone, offspring)
+
+        # Apply crossover and mutation on the offspring
+        offspring = algorithms.varAnd(offspring, toolbox, IND_CROSSOVER_RATE, IND_MUTATION_RATE)
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # The population is entirely replaced by the offspring
+        pop[:] = offspring
+        print(stats.compile(pop))
 
 
-with open("results/results_ea_{}.txt".format(datetime.now().strftime("%H-%M-%S")), "w") as fw:
-    fw.write(str(hof))
-    fw.write(str(logbook))
+# with open("results/results_ea_{}.txt".format(datetime.now().strftime("%H-%M-%S")), "w") as fw:
+#    fw.write(str(hof))
+#    fw.write(str(logbook))
