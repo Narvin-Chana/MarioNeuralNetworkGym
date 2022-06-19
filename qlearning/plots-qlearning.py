aac7d4cd021102a8326e76cb9c66e0a2a1c285e2
Narvin Chana <narvin.chana@etu.univ-tours.fr>
2022-06-19 19:45:48 +0200

    fix: fixed order of pickle in plots-qlearning.py

--- qlearning/plots-qlearning.py
+++ qlearning/plots-qlearning.py
@@ -1,11 +1,11 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open("results/qlearning-stats(1).dat", "rb") as fr:
+with open("results/qlearning-stats.dat", "rb") as fr:
     log = pickle.load(fr)
 
-time = log[0]
-val = log[1]
+val = log[0]
+time = log[1]
 
 plt.plot(time, val)
 plt.show()
